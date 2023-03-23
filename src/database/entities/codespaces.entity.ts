1c3e7868f70396457ea102a42c36ea1618ae27fd
FernasG <fernandogoul7@gmail.com>
2023-03-22 23:46:19 -0300

    feat: add codespaces migration and entity

---
+++ src/database/entities/codespaces.entity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Codespaces {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    name: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    description: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    filename: string;
+
+    @Column({ type: 'enum', enumName: 'lang', enum: ['python', 'javascript'] })
+    language: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at: Date;
+}
--- src/database/entities/index.ts
+++ src/database/entities/index.ts
@@ -1,3 +1,4 @@
 export * from "./users.entity";
 export * from "./roles.entity";
+export * from "./codespaces.entity";
 export * from "./users_roles.entity";
