2174dcef148d4f992693ce8d6603d9ecf5a1742e
FernasG <fernandogoul7@gmail.com>
2023-03-22 23:58:03 -0300

    feat: add users codespsaces migrations and entities

--- src/database/entities/index.ts
+++ src/database/entities/index.ts
@@ -1,4 +1,5 @@
+export * from "./users_codespaces.entity";
+export * from "./users_roles.entity";
+export * from "./codespaces.entity";
 export * from "./users.entity";
 export * from "./roles.entity";
-export * from "./codespaces.entity";
-export * from "./users_roles.entity";
